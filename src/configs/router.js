741e9704921f9e4bf7462711e3525dde3f753e86
snake <1819747081@qq.com>
2019-01-24 11:55:27 +0800

    删库跑路

--- src/configs/router.js
+++ src/configs/router.js
@@ -134,18 +134,4 @@ const router = new VueRouter({
   routes // (缩写) 相当于 routes: routes
 });
 
-// 路由守卫
-router.beforeEach((to, from, next) => {
-  let token = localStorage.getItem("token");
-  // 如果token是123456的话进去目标页面，否则返回'/sign'页面
-  if (token == 123456 || to.path == '/login') {
-      next()
-  } else {
-      // console.log(to.path,from)
-      // 如果失败跳转登录页面
-      router.push({
-          name: "login"
-      })
-  }
-})
 export default router;
--- src/pages/Wechat.vue
+++ src/pages/Wechat.vue
@@ -38,17 +38,17 @@ export default {
   },
   methods: {
     autoLogin() {
+      console.log(localStorage.getItem("token"));
       this.$axios({
         method: "post",
-        headers: {
-          token: localStorage.getItem("token")
-        },
-        url: "http://39.96.28.141:3000/users/autoLogin"
+        url: "http://39.96.28.141:3000/users/autoLogin",
+        data: this.$qs.stringify({
+          isToken:localStorage.getItem("token")
+        })
       }).then(res => {
-        console.log(res.status);
+        console.log(res.data.status);
         let fn = {
           true: () => {
-            //   localStorage.setItem("token",res.data.token);
             console.log(666);
           },
           false: () => {
@@ -59,72 +59,8 @@ export default {
       });
     }
   },
-  beforeCreate() {
+  created() {
     this.autoLogin();
   }
-};
-</script>
-<template>
-  <div>
-    <Xheader/>
-    <Xbanner/>
-    <Xnav/>
-    <Xhot/>
-    <Xtuijian/>
-    <Xphone/>
-    <Xxiala/>
-  </div>
-</template>
-<script>
-// 引用组件
-import Xheader from "../components/Xheader.vue";
-import Xbanner from "../components/Xbanner.vue";
-import Xnav from "../components/Xnav.vue";
-import Xhot from "../components/Xhot.vue";
-import Xtuijian from "../components/Xtuijian.vue";
-import Xphone from "../components/Xphone.vue";
-import Xxiala from "../components/Xxiala.vue";
-
-export default {
-  data() {
-    return {
-      name: "微信"
-    };
-  },
-  components: {
-    // 注册组件
-    // 引用组件，在注册，然后在template使用
-    Xheader,
-    Xbanner,
-    Xnav,
-    Xhot,
-    Xtuijian,
-    Xphone,
-    Xxiala
-  },
-  methods: {
-    login() {
-      this.$axios({
-        method: "post",
-        headers: {
-          token: localStorage.getItem("token")
-        },
-        url: "http://39.96.28.141:3000/users/autoLogin"
-      }).then(res => {
-        console.log(res.status);
-        let fn = {
-          true: () => {
-            //   localStorage.setItem("token",res.data.token);
-            console.log(666);
-          },
-          false: () => {
-            this.$router.push({ name: "login" });
-          }
-        };
-        fn[res.data.status]();
-      });
-    }
-  },
-  destroyed() {}
 };
 </script>
